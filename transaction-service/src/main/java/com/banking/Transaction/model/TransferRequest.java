3f8ba37400b92eadc8d9b5af039ca5e256cfa314
rohitgapat <rohitgapat233@gmail.com>
2026-09-21 20:53:34 +0530

    TransferRequest add in transaction service

---
+++ transaction-service/src/main/java/com/banking/Transaction/model/TransferRequest.java
@@ -0,0 +1,24 @@
+package com.banking.Transaction.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TransferRequest {
+
+    @NotBlank(message = "From account number is required")
+    private String fromAccountNumber;
+
+    @NotBlank(message = "To account number is required")
+    private String toAccountNumber;
+
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be greater than zero")
+    private Double amount;
+}
